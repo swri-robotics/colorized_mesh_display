0248f3ad9ffaad0fa9ceedd27466a4ab40477a42
mripperger <michael.ripperger@swri.org>
2018-08-02 17:17:07 -0500

    Added colorized mesh class renderable by ogre

---
+++ include/colorized_mesh_display/colorized_mesh_visual.h
@@ -0,0 +1,56 @@
+#ifndef COLORIZED_MESH_VISUAL_H
+#define COLORIZED_MESH_VISUAL_H
+
+#include <colorized_mesh_display/ColorizedMesh.h>
+
+namespace Ogre
+{
+class Vector3;
+class Quaternion;
+class SceneNode;
+class SceneManager;
+class ManualObject;
+class Entity;
+}
+
+namespace rviz
+{
+class MeshShape;
+}
+
+namespace colorized_mesh_display
+{
+
+class ColorizedMeshShape;
+
+class ColorizedMeshVisual
+{
+public:
+
+  ColorizedMeshVisual(Ogre::SceneManager* scene_manager,
+                      Ogre::SceneNode* parent_node = NULL);
+
+  virtual ~ColorizedMeshVisual();
+
+  void visualizeMesh(const ColorizedMesh& msg);
+
+  void setFramePosition(const Ogre::Vector3& position);
+
+  void setFrameOrientation(const Ogre::Quaternion& orientation);
+
+private:
+
+  Ogre::SceneManager* scene_manager_;
+
+  Ogre::SceneNode* frame_node_;
+
+  Ogre::ManualObject* manual_object_;
+
+  Ogre::Entity* entity_;
+
+  std::string material_name_;
+};
+
+} // namespace colorized_mesh_display
+
+#endif // COLORIZED_MESH_VISUAL_H

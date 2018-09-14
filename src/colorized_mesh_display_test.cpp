c7c546d288038827d2e83f5c7d88e49f2b1c98f0
mripperger <michael.ripperger@swri.org>
2018-09-14 12:15:18 -0500

    Using utils library in test node to convert PolygonMesh

--- src/colorized_mesh_display_test.cpp
+++ src/colorized_mesh_display_test.cpp
@@ -1,4 +1,5 @@
 #include <colorized_mesh_display/ColorizedMeshStamped.h>
+#include <colorized_mesh_display/utils.h>
 #include <pcl/io/ply_io.h>
 #include <ros/ros.h>
 #include <random>
@@ -6,58 +7,6 @@
 float generateRandomFloat()
 {
   return float(rand()) / float(INT_MAX);
-}
-
-colorized_mesh_display::ColorizedMeshStamped toColorizedMesh(const pcl::PolygonMesh& in,
-                                                             const std::string& base_frame)
-{
-  colorized_mesh_display::ColorizedMeshStamped out;
-  out.header.frame_id = base_frame;
-  out.header.stamp = ros::Time::now();
-
-  pcl::PointCloud<pcl::PointNormal> vertex_cloud;
-  pcl::fromPCLPointCloud2(in.cloud, vertex_cloud);
-
-  // Add the vertex information
-  for(std::size_t i = 0; i < vertex_cloud.points.size(); ++i)
-  {
-    const pcl::PointNormal& pt = vertex_cloud.points[i];
-    // Add the vertex
-    geometry_msgs::Point32 vertex;
-    vertex.x = pt.x;
-    vertex.y = pt.y;
-    vertex.z = pt.z;
-    out.mesh.vertices.push_back(vertex);
-
-    // Add the normal
-    geometry_msgs::Vector3 normal;
-    normal.x = 0; //pt.normal_x;
-    normal.y = 0; //pt.normal_y;
-    normal.z = 1; //pt.normal_z;
-    out.mesh.vertex_normals.push_back(normal);
-
-    // Add the color
-    std_msgs::ColorRGBA color;
-    color.r = generateRandomFloat();
-    color.g = generateRandomFloat();
-    color.b = generateRandomFloat();
-    color.a = 1.0f;
-    out.mesh.vertex_colors.push_back(color);
-  }
-
-  // Add the triangle information
-  for(std::size_t i = 0; i < in.polygons.size(); ++i)
-  {
-    const pcl::Vertices& poly = in.polygons[i];
-    for(std::size_t j = 0; j < poly.vertices.size() - 2; ++j)
-    {
-      shape_msgs::MeshTriangle triangle;
-      triangle.vertex_indices = {{poly.vertices[0], poly.vertices[j + 1], poly.vertices[j + 2]}};
-      out.mesh.triangles.push_back(std::move(triangle));
-    }
-  }
-
-  return out;
 }
 
 int main(int argc, char** argv)
@@ -91,7 +40,10 @@ int main(int argc, char** argv)
 
   srand(seed);
 
-  colorized_mesh_display::ColorizedMeshStamped colorized_mesh = toColorizedMesh(mesh, base_frame);
+  colorized_mesh_display::ColorizedMeshStamped colorized_mesh;
+  colorized_mesh.mesh = colorized_mesh_display::fromPCLPolygonMesh(mesh);
+  colorized_mesh.header.frame_id = base_frame;
+  colorized_mesh.header.stamp = ros::Time::now();
 
   ros::Publisher pub = nh.advertise<colorized_mesh_display::ColorizedMeshStamped>("colorized_mesh", 1, true);
   pub.publish(colorized_mesh);
